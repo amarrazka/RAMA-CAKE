3549a33e83d2dbd9faf1024add395b5f280f672b
amarrazka <rivaldo4341@gmail.com>
2026-01-03 00:43:47 +0700

    Initial commit RAMA-CAKE

---
+++ about.php
@@ -0,0 +1,19 @@
+<?php 
+	include 'header.php';
+ ?>
+
+
+<div class="container" style="padding-bottom: 300px;">
+	<h2 style=" width: 100%; border-bottom: 4px solid #600803ff"><b>Tentang Kami</b></h2>
+	<p class="text-justify">RAMA-Cake & Bakery adalah salah satu pelopor bisnis roti modern di Indonesia. Didirikan pada tahun 1978 dan saat ini dikelola di bawah naungan PT. Mustika Citra Rasa, RAMA-Cake & Bakery terus berkembang menjadi jaringan toko roti terkemuka di Indonesia.</p>
+<p>Saat ini, RAMA-Cake & Bakery memiliki 22 cabang (dapur pusat) yang mengelola lebih dari 400 outlet yang tersebar di berbagai kota besar seperti Jabodetabek, Bandung, Surabaya, Lampung, Batam, Pekanbaru, Makassar, Manado, Bali, Solo, Semarang, Balikpapan, dan Samarinda, serta terus melakukan ekspansi ke kota-kota lain di seluruh Indonesia.</p>
+	<p>
+RAMA-Cake & Bakery  adalah salah satu pelopor pertama dalam bisnis roti modern di Indonesia. Didirikan pada tahun 1978, saat ini dikelola di bawah PT. Mustika Citra Rasa. Produk kami sehat, bergizi, dan terjangkau oleh semua orang.</p>
+</div>
+
+
+
+
+ <?php 
+	include 'footer.php';
+ ?>
---
+++ footer.php
@@ -0,0 +1,31 @@
+
+	<footer style="border-top: 4px solid #600803ff;  ">
+		<div class="container" style="padding-bottom: 50px;">
+			<div class="row">
+				<div class="col-md-4">
+					<h3 style="color: #600803ff"><b>RAMA-CAKE BAKERY</b></h3>
+					<p>Jl.SindangSari 2 No.10</p>
+					<p><i class="glyphicon glyphicon-earphone"></i> +6282129090785</p>
+					<p><i class="glyphicon glyphicon-envelope"></i> rama-cake@gmail.com</p>
+				</div>
+				<div class="col-md-4">
+					<h5><b>Menu</b></h5>
+					<p><a href=""  style="color: #000">Produk</a></p>
+					<p><a href=""  style="color: #000">Tentang kami</a></p>
+					<p><a href=""  style="color: #000">Hubungi Kami</a></p>
+				</div>
+
+				<div class="col-md-4">
+					
+				</div>
+			</div>
+
+		</div>
+
+		<div class="copy" style="background-color: #600803ff; padding: 5px; color: #fff; text-align: center;">
+			<span>RAMA-CAKE</span>
+		</div>
+	</footer>
+
+</body>
+</html>
